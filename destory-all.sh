31af5af433fa249d2c1e1c90c1b14001fd1357f9
Elton Tang <etang315@gmail.com>
2015-11-29 21:34:14 -0600

    updating what needed to remove

--- destory-all.sh
+++ destory-all.sh
@@ -2,3 +2,21 @@
 
 aws ec2-terminate-instances ami-d05e75b8
 aws elb delete-load-balancer --load-balancer-name itmo-444-et-lb
+
+
+# remove cloud watch metris
+# remove autoscaling group
+# Remove RDS instance
+# delete Mysql DB
+# remove stuff from application setup
+# uninstall git
+# uninstall php5
+# uninstall php5-curl
+# uninstall mysql-client
+#uninstall curl
+#uinstall php5-mysql
+# uninstall apache 
+#uninstall php5 php5-imagick
+
+
+#remove html
