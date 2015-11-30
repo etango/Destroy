10934a93a6728a416a41867db0c71b89e946f100
Elton Tang <etang315@gmail.com>
2015-11-29 21:40:06 -0600

    added removing enivronement

--- destory-all.sh
+++ destory-all.sh
@@ -4,19 +4,25 @@ aws ec2-terminate-instances ami-d05e75b8
 aws elb delete-load-balancer --load-balancer-name itmo-444-et-lb
 
 
+
+
 # remove cloud watch metris
 # remove autoscaling group
 # Remove RDS instance
 # delete Mysql DB
 # remove stuff from application setup
-# uninstall git
-# uninstall php5
-# uninstall php5-curl
-# uninstall mysql-client
-#uninstall curl
-#uinstall php5-mysql
-# uninstall apache 
-#uninstall php5 php5-imagick
+
+
+sudo apt-get remove -y apache2
+sudo apt-get remove -y git
+sudo apt-get remove -y php5
+sudo apt-get remove -y php5-curl
+sudo apt-get remove -y mysql-client
+sudo apt-get remove -y curl
+sudo apt-get remove -y php5-mysql
+sudo apt-get remove -y php5 php5-imagick
+
+
 
 
 #remove html
